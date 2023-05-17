c2001440b04aa6688314069d912dcfdafa746c95
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-05-17 13:29:52 +0300

    made page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, MoviesList } from '../components/Layout/Layout.styled';
+import { trendingmoviesRequest } from '../services/API';
+
 const Home = () => {
-  return <>123</>;
+  const [moviesTrand, setMoviesTrend] = useState([]);
+
+  useEffect(() => {
+    (async function trendingMovies() {
+      const res = await trendingmoviesRequest();
+      const movies = res.data.results;
+      if (movies) {
+        setMoviesTrend(movies);
+      }
+    })();
+
+    return () => {};
+  }, []);
+
+  return (
+    <Container>
+      <h1>Trending today</h1>
+      <MoviesList>
+        {moviesTrand.map(({ id, name, title }) => (
+          <li key={id}>
+            <Link to={`movies/${id}`} key={id}>
+              {title} {name}
+            </Link>
+          </li>
+        ))}
+      </MoviesList>
+    </Container>
+  );
 };
 
 export default Home;
--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,4 +1,17 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const API_KEY = '309df4a19ba58fdd322dab99a0059b3e';
+const KEY = '309df4a19ba58fdd322dab99a0059b3e';
+
+export async function trendingmoviesRequest() {
+  try {
+    const response = await axios.get(`trending/movie/day?`, {
+      params: {
+        api_key: KEY,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log('error = ', error);
+  }
+}
